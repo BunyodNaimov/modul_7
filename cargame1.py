a98e7f131aebee680463bea8dffd6e2ea1ab4281
bunyod <naimov.bunyod2021@gmail.com>
2023-12-26 14:54:31 +0500

    update

--- cargame1.py
+++ cargame1.py
@@ -1,4 +1,4 @@
-#tutorial: https://www.youtube.com/watch?v=W-QOtdD3qx4
+# tutorial: https://www.youtube.com/watch?v=W-QOtdD3qx4
 
 import pygame
 from pygame.locals import *
@@ -6,11 +6,11 @@ import random
 
 # shape parameters
 size = width, height = (800, 800)
-road_w = int(width/1.6)
-roadmark_w = int(width/80)
+road_w = int(width / 1.6)
+roadmark_w = int(width / 80)
 # location parameters
-right_lane = width/2 + road_w/4
-left_lane = width/2 - road_w/4
+right_lane = width / 2 + road_w / 4
+left_lane = width / 2 - road_w / 4
 # animation parameters
 speed = 1
 
@@ -29,42 +29,42 @@ pygame.display.update()
 
 # load player vehicle
 car = pygame.image.load("car.png")
-#resize image
-#car = pygame.transform.scale(car, (250, 250))
+# resize image
+# car = pygame.transform.scale(car, (250, 250))
 car_loc = car.get_rect()
-car_loc.center = right_lane, height*0.8
+car_loc.center = right_lane, height * 0.8
 
 # load enemy vehicle
 car2 = pygame.image.load("otherCar.png")
 car2_loc = car2.get_rect()
-car2_loc.center = left_lane, height*0.2
+car2_loc.center = left_lane, height * 0.2
 
 counter = 0
-# game loop
+# игровой цикл
 while running:
     counter += 1
 
-    # increase game difficulty overtime
+    # увеличивайте сложность игры со временем
     if counter == 5000:
         speed += 0.15
         counter = 0
         print("level up", speed)
 
-    # animate enemy vehicle
+    # анимировать вражескую технику
     car2_loc[1] += speed
     if car2_loc[1] > height:
         # randomly select lane
-        if random.randint(0,1) == 0:
+        if random.randint(0, 1) == 0:
             car2_loc.center = right_lane, -200
         else:
             car2_loc.center = left_lane, -200
 
-    # end game logic
+    # логика конца игры
     if car_loc[0] == car2_loc[0] and car2_loc[1] > car_loc[1] - 250:
         print("GAME OVER! YOU LOST!")
         break
 
-    # event listeners
+    # прослушиватели событий
     for event in pygame.event.get():
         if event.type == QUIT:
             # collapse the app
@@ -72,31 +72,31 @@ while running:
         if event.type == KEYDOWN:
             # move user car to the left
             if event.key in [K_a, K_LEFT]:
-                car_loc = car_loc.move([-int(road_w/2), 0])
+                car_loc = car_loc.move([-int(road_w / 2), 0])
             # move user car to the right
             if event.key in [K_d, K_RIGHT]:
-                car_loc = car_loc.move([int(road_w/2), 0])
-    
+                car_loc = car_loc.move([int(road_w / 2), 0])
+
     # draw road
     pygame.draw.rect(
         screen,
         (50, 50, 50),
-        (width/2-road_w/2, 0, road_w, height))
+        (width / 2 - road_w / 2, 0, road_w, height))
     # draw centre line
     pygame.draw.rect(
         screen,
         (255, 240, 60),
-        (width/2 - roadmark_w/2, 0, roadmark_w, height))
+        (width / 2 - roadmark_w / 2, 0, roadmark_w, height))
     # draw left road marking
     pygame.draw.rect(
         screen,
         (255, 255, 255),
-        (width/2 - road_w/2 + roadmark_w*2, 0, roadmark_w, height))
+        (width / 2 - road_w / 2 + roadmark_w * 2, 0, roadmark_w, height))
     # draw right road marking
     pygame.draw.rect(
         screen,
         (255, 255, 255),
-        (width/2 + road_w/2 - roadmark_w*3, 0, roadmark_w, height))
+        (width / 2 + road_w / 2 - roadmark_w * 3, 0, roadmark_w, height))
 
     # place car images on the screen
     screen.blit(car, car_loc)
