43f3dbba6b7940fa78a0b6210f9878453fe56500
bunyod <naimov.bunyod2021@gmail.com>
2024-01-06 14:53:26 +0500

    update

--- cargame1.py
+++ cargame1.py
@@ -40,44 +40,49 @@ car2_loc = car2.get_rect()
 car2_loc.center = left_lane, height * 0.2
 
 counter = 0
-# игровой цикл
+# Игровой цикл
 while running:
     counter += 1
 
-    # увеличивайте сложность игры со временем
+    # Увеличение сложности игры со временем
     if counter == 5000:
         speed += 0.15
         counter = 0
-        print("level up", speed)
+        print("Уровень повышен", speed)
 
-    # анимировать вражескую технику
-    car2_loc[1] += speed
-    if car2_loc[1] > height:
-        # randomly select lane
+    # Анимация вражеской машины
+    car2_loc.y += speed
+    if car2_loc.y > height:
+        # Случайный выбор полосы
         if random.randint(0, 1) == 0:
             car2_loc.center = right_lane, -200
         else:
             car2_loc.center = left_lane, -200
 
-    # логика конца игры
-    if car_loc[0] == car2_loc[0] and car2_loc[1] > car_loc[1] - 250:
-        print("GAME OVER! YOU LOST!")
+    # Логика завершения игры
+    if car_loc.colliderect(car2_loc):
+        print("ИГРА ОКОНЧЕНА! ВЫ ПРОИГРАЛИ!")
         break
 
-    # прослушиватели событий
+    # Обработчики событий
     for event in pygame.event.get():
         if event.type == QUIT:
-            # свернуть приложение
             running = False
         if event.type == KEYDOWN:
-            # переместить машину пользователя влево
             if event.key in [K_a, K_LEFT]:
-                car_loc = car_loc.move([-int(road_w / 2), 0])
-            # переместить машину пользователя вправо
+                # Переместить машину игрока влево
+                car_loc.x -= int(road_w / 2)
+                # Ограничение позиции машины в пределах дороги
+                if car_loc.left < width / 2 - road_w / 2 + roadmark_w * 2:
+                    car_loc.left = width / 2 - road_w / 2 + roadmark_w * 2
             if event.key in [K_d, K_RIGHT]:
-                car_loc = car_loc.move([int(road_w / 2), 0])
-
+                # Переместить машину игрока вправо
+                car_loc.x += int(road_w / 2)
+                # Ограничение позиции машины в пределах дороги
+                if car_loc.right > width / 2 + road_w / 2 - roadmark_w * 3:
+                    car_loc.right = width / 2 + road_w / 2 - roadmark_w * 3
     # draw road
+    screen.fill((60, 220, 0))  # Очистка экрана
     pygame.draw.rect(
         screen,
         (50, 50, 50),
