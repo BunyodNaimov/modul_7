69a8abde836235257a512fe5a208d46310a309cb
bunyod <naimov.bunyod2021@gmail.com>
2024-01-23 14:09:03 +0500

    update

--- snake.py
+++ snake.py
@@ -1,5 +1,6 @@
 import pygame
 import random
+import time
 
 pygame.init()
 
@@ -22,14 +23,29 @@ snake_body = [
     [100, 50],
     [90, 50],
     [80, 50],
-    [70, 50]
+    [70, 50],
+    [60, 50],
+    [50, 50],
+    [40, 50],
+    [30, 50],
+    [20, 50],
+    [10, 50],
 ]
 
-food_position = [random.randint(1, (WIDTH // 10)) * 10, random.randint(1, (HEIGHT // 10)) * 10]
+score = 0
+font = pygame.font.SysFont(None, 36)
+
+food_position = [random.randint(1, (WIDTH // 10)) * 10,
+                 random.randint(1, (HEIGHT // 10)) * 10]
 
 
 def game_over():
-    pygame.quit()
+    my_font = pygame.font.SysFont(None, 80)
+    game_over_text = my_font.render(f"Your Score: {score}", True, "red")
+    screen.blit(game_over_text, (130, 280))
+    pygame.display.flip()
+    time.sleep(2)
+    exit()
 
 
 running = True
@@ -68,19 +84,34 @@ while running:
         snake_position[0] += 10
 
     snake_body.insert(0, list(snake_position))
-    snake_body.pop()
+
+    if snake_position[0] == food_position[0] and snake_position[1] == food_position[1]:
+        score += 1
+        food_position = [random.randint(1, (WIDTH // 10)) * 10,
+                         random.randint(1, (HEIGHT // 10)) * 10]
+    else:
+        snake_body.pop()
 
     screen.fill("black")
     for pos in snake_body:
-        pygame.draw.rect(screen, "green", pygame.Rect(pos[0], pos[1], 20, 20))
+        pygame.draw.rect(screen, "green", pygame.Rect(pos[0], pos[1], 10, 10))
 
-    pygame.draw.rect(screen, "White", pygame.Rect(food_position[0], food_position[1], 20, 20))
+    pygame.draw.rect(screen, "White", pygame.Rect(food_position[0], food_position[1], 10, 10))
 
     if snake_position[0] < 0 or snake_position[0] > WIDTH - 10:
         game_over()
     if snake_position[1] < 0 or snake_position[1] > HEIGHT - 10:
         game_over()
 
+    for block in snake_body[1:]:
+        if snake_position[0] == block[0] and snake_position[1] == block[1]:
+            game_over()
+
+    text = font.render(f"Score: {score}", 1, "white")
+    screen.blit(text, (10, 10))
+
+    speed_text = font.render(f"Speed: {snake_speed + score}", 1, "white")
+    screen.blit(speed_text, (10, 40))
     pygame.display.update()
-    fps.tick(snake_speed)
+    fps.tick(snake_speed + score)
 pygame.quit()
